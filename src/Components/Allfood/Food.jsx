9a5a8d246a0ce12d2c87895b4c18e313cc9d4106
unknown <mdisrafil61@gmail.com>
2023-05-04 17:09:04 +0600

    new commit

--- src/Components/Allfood/Food.jsx
+++ src/Components/Allfood/Food.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './food.css';
 import { Link } from 'react-router-dom';
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Food = ({food}) => {
     const {id,likes,name,number_of_recipes,picture_url,years_of_experience,recipes} = food;
@@ -9,7 +10,8 @@ const Food = ({food}) => {
     return (
         <div className='food-content w-96 h-full'>
             <div className="card bg-base-100 shadow-xl food-img">
-                        <img src={picture_url} alt="Shoes" className="rounded-xl" />
+            {/* <img loading="lazy" src={picture_url} alt="Shoes" className="rounded-xl" /> */}   
+                <LazyLoadImage src={picture_url} alt="Shoes" className="rounded-xl"/>
                  <div className="card-body items-center text-center">
                           <h2 className="card-title">Chef Name:{name}</h2>
                             <h4>Years of experience:{years_of_experience}</h4>

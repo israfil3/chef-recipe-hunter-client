4c43b8fe239a9cfd787706aceb3c69989f7b28f9
unknown <mdisrafil61@gmail.com>
2023-05-05 20:32:25 +0600

    tost done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -51,6 +51,7 @@ const Header = () => {
 
               )}  
            </div>
+           
         </div>   
         </>
     );
--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
 
 const Recipe = () => {
+    const notify = () => toast("Add to favorite",{
+        position: toast.POSITION.TOP_CENTER
+    });
     const {id} = useParams;
     const newData = useLoaderData();
     console.log(newData)
@@ -11,10 +17,12 @@ const Recipe = () => {
             <figure><img className='w-[40%] mt-10' src={newData.selectedData.picture_url} alt="Album"/></figure>
             <div className="card-body px-40">
                 <h2 className="card-title text-2xl text-center">Name:{newData.selectedData.name}</h2>
-                <h3><span className='text-2xl'>Description:</span> {newData.selectedData.description}</h3>
-                <p><span className='text-2xl'>Like:</span>  {newData.selectedData.likes}</p>
-                <p><span className='text-2xl'>Total Recipes items: </span> {newData.selectedData.recipes.length}</p>
-                <p><span className='text-2xl'>Year of experience:</span> {newData.selectedData.years_of_experience}</p>
+                <h3 className='text-2xl'><span className='text-2xl'>Description:</span> {newData.selectedData.description}</h3>
+                <p className='text-2xl'><span className='text-2xl'>Like:</span>  {newData.selectedData.likes}</p>
+                <p className='text-2xl'><span className='text-2xl'>Total Recipes items: </span> {newData.selectedData.recipes.length}</p>
+                <p className='text-2xl'> <span className='text-2xl'>Year of experience:</span> {newData.selectedData.years_of_experience}</p>
+                <p className='text-2xl'><span className='text-2xl'>Ratting:</span> 4.8</p>
+                <p className='text-2xl'><span className='text-2xl'>Favorite:</span>true</p>
                 {
                      
                     newData.selectedData.recipes.map(dk => <div className="">
@@ -26,6 +34,10 @@ const Recipe = () => {
                         )
                 }
                 <div className="card-actions justify-end">
+                </div>
+                <div>
+                    <button onClick={notify} className="btn btn-outline btn-error w-52">favorite</button>
+                    <ToastContainer />
                 </div>
             </div>
             </div>
